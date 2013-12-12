074cc2acf4ff22980c918aedf7c650927782493b
Jin Wang <owenwjowenwj@gmail.com>
2013-12-12 15:24:52 +1100

    Add shim for local view. rewrite application to enable header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 JoyCorn::Application.routes.draw do
-  get "static_pages/home"
+  get "static_pages/index"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
