fe507d0e04d81b09218f7c0b050f64d880b0f88b
Jin Wang <owenwjowenwj@gmail.com>
2013-12-12 13:49:22 +1100

    Add static_pages_controller with actions index and help.

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+	# describe "Get /static_pages" do
+	subject {page}
+	let(:base_title){""}
+
+	describe "Home page" do
+		before {visit root_path}
+		let(:heading) {}
+	end
+end
