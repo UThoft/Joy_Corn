b03f226aa1efa0ba201dd137fe84a445a1733fd1
Jin Wang <owenwjowenwj@gmail.com>
2013-12-18 14:36:55 +1100

    * Adjust the post-view image size.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 class StaticPagesController < ApplicationController
-  helper_method :default_post_panel, :next_post_panel
+  helper_method :default_post_panel, :next_post_panel, :search_post_panel
 
   def index
+    $current_index = 0
+    $search_type = 'topic'
+    $keyword = 'default'
   end
 
   def help
@@ -10,9 +13,10 @@ class StaticPagesController < ApplicationController
   def next
   end
 
-  def post panel
+  def search
+    $current_index = 0
+    $search_type = params[:search_type]
     $keyword = params[:keyword]
-    $is_type = params[:is_type]
   end
 
   def default_post_panel
@@ -26,6 +30,7 @@ class StaticPagesController < ApplicationController
     temp = return_post(3, $current_index)
     $current_index = $current_index + 3
     temp
+  #  need to consider if there is search condition?
   end
 
   def return_post(post_number, start_point)
@@ -34,12 +39,22 @@ class StaticPagesController < ApplicationController
     elsif $is_type == 1
       Post.where(media_type: @keyword).order(created_at: :desc).limit(post_number).offset(start_point)
     else
-      search_initial
+      search_post_panel
     end
   end
 
-  def search_initial
+  def search_post_panel
+
+  end
+
+  def search_post_panel_by(search_type, keyword)
   #  Handle the search keyword to generate proper results.
+
+
+  end
+
+  def return_post_search_by(search_type, keyword, post_number, start_point)
+
   end
 
   def home_button
