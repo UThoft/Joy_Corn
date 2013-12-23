31301b639052df60db36dbcd3a2c0a45059eb040
Jin Wang <owenwjowenwj@gmail.com>
2013-12-23 18:33:09 +1100

    add google analytics into the project

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,13 @@ module JoyCorn
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     #config.time_zone = 'Melbourne'
+    # If you set config.assets.initialize_on_precompile to false,
+    # be sure to test rake assets:precompile locally before deploying.
+    # It may expose bugs where your assets reference application objects or methods,
+    # since those are still in scope in development mode regardless of the value of this flag.
+    # Changing this flag also affects engines. Engines can define assets for precompilation as well.
+    # Since the complete environment is not loaded, engines (or other gems) will not be loaded,
+    # which can cause missing assets.
 
     config.assets.initialize_on_precompile = false
 
